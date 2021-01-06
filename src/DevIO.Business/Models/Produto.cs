96e3bfe1be28d0789ffabbe34293cb051f2e8588
Gabriel Coronin <gabrielcoronin@gmail.com>
2021-01-06 12:56:28 -0300

    feat(Data): criação e configuração do banco de dados

--- src/DevIO.Business/Models/Produto.cs
+++ src/DevIO.Business/Models/Produto.cs
@@ -5,7 +5,7 @@ namespace AspMvcBasica.Models
     public class Produto : Entity
     {
         public Guid FornecedorId { get; set; }
-        public string Name { get; set; }
+        public string Nome { get; set; }
         public string Descricao { get; set; }
         public string Imagem { get; set; }
         public decimal Valor { get; set; }
---
+++ src/DevIO.Data/Context/MeuDbContext.cs
@@ -0,0 +1,27 @@
+﻿using AspMvcBasica.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace DevIO.Data.Context
+{
+    public class MeuDbContext : DbContext
+    {
+        public MeuDbContext(DbContextOptions options) : base(options) {  }
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(MeuDbContext).Assembly);
+
+            foreach (var relashionship in modelBuilder.Model.GetEntityTypes()
+                .SelectMany(e => e.GetForeignKeys())) 
+                relashionship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
