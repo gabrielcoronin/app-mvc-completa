dd80312547cc41c6c5c90fd20b8328a8ce97443e
Gabriel Coronin <gabrielcoronin@gmail.com>
2021-01-06 12:12:55 -0300

    Add project files.

---
+++ src/DevIO.Business/Models/Produto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AspMvcBasica.Models
+{
+    public class Produto : Entity
+    {
+        public Guid FornecedorId { get; set; }
+        public string Name { get; set; }
+        public string Descricao { get; set; }
+        public string Imagem { get; set; }
+        public decimal Valor { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public bool Ativo { get; set; }
+
+        // EF Relation
+        public Fornecedor Fornecedor { get; set; }
+
+    }
+}
