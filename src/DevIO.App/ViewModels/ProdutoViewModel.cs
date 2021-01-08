d23bf4ab898109b7fa53193ff68e93ce345575cd
Gabriel Coronin <gabrielcoronin@gmail.com>
2021-01-08 10:24:00 -0300

    feat(views): customizando as views

--- src/DevIO.App/ViewModels/ProdutoViewModel.cs
+++ src/DevIO.App/ViewModels/ProdutoViewModel.cs
@@ -18,13 +18,15 @@ namespace DevIO.App.ViewModels
 
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         [StringLength(200, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
-        public string Name { get; set; }
+        public string Nome { get; set; }
 
         [DisplayName("Descrição")]
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         [StringLength(1000, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
         public string Descricao { get; set; }
-        //public IFormFile ImagemUpload { get; set; }
+
+        [DisplayName("Imagem do Produto")]
+        public IFormFile ImagemUpload { get; set; }
         public string Imagem { get; set; }
 
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
